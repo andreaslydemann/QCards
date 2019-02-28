b728adaea70c9494239d0bfe4dcb44ff80668cdd
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-02-28 09:29:33 +0100

    Add missing files

---
+++ Platform/Platform.h
@@ -0,0 +1,19 @@
+//
+//  Platform.h
+//  Platform
+//
+//  Created by Andreas Lüdemann on 27/02/2019.
+//  Copyright © 2019 Andreas Lüdemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Platform.
+FOUNDATION_EXPORT double PlatformVersionNumber;
+
+//! Project version string for Platform.
+FOUNDATION_EXPORT const unsigned char PlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Platform/PublicHeader.h>
+
+
