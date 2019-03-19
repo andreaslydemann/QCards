7ddb1489f8a2883d2e4326df8f506151786c34df
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-03-19 22:46:35 +0100

    First steps of implementing DecksNavigator

---
+++ QCards/Scenes/AllDecks/DecksNavigator.swift
@@ -0,0 +1,32 @@
+//
+//  DecksNavigator.swift
+//  QCards
+//
+//  Created by Andreas Lüdemann on 19/03/2019.
+//  Copyright © 2019 Andreas Lüdemann. All rights reserved.
+//
+
+import UIKit
+import Domain
+
+protocol DecksNavigator {
+    func toDecks()
+}
+
+class DefaultDecksNavigator: DecksNavigator {
+    private let navigationController: UINavigationController
+    private let services: UseCaseProvider
+    
+    init(services: UseCaseProvider,
+         navigationController: UINavigationController) {
+        self.services = services
+        self.navigationController = navigationController
+    }
+    
+    func toDecks() {
+        //let vc = storyBoard.instantiateViewController(ofType: DecksViewController.self)
+        vc.viewModel = DecksViewModel(useCase: services.makeDecksUseCase(),
+                                      navigator: self)
+        navigationController.pushViewController(vc, animated: true)
+    }
+}
