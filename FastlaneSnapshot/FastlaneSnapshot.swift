d5107090835747c72e8382d64b1cc6a285df8e4f
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-08-11 23:33:28 +0200

    Improvement to fastlane snapshots

--- FastlaneSnapshot/FastlaneSnapshot.swift
+++ FastlaneSnapshot/FastlaneSnapshot.swift
@@ -21,13 +21,49 @@ class FastlaneSnapshot: XCTestCase {
     }
     
     func testGenerateScreenshots() {
+
+        let deckTitle = localizedString(key: "Snapshot.DeckTitle")
         
-        let deckTitle = localizedString("Snapshot.DeckTitle")
-        let cardTitle = localizedString("Snapshot.CardTitle")
-        let cardContent = localizedString("Snapshot.CardContent")
+        let cardTitle1 = localizedString(key: "Snapshot.CardTitle1")
+        let cardContent1 = localizedString(key: "Snapshot.CardContent1")
+        
+        let cardTitle2 = localizedString(key: "Snapshot.CardTitle2")
+        let cardContent2 = localizedString(key: "Snapshot.CardContent2")
+        
+        let cardTitle3 = localizedString(key: "Snapshot.CardTitle3")
+        let cardContent3 = localizedString(key: "Snapshot.CardContent3")
         
         let app = XCUIApplication()
         
+        setTimePerCard(app)
+        createDeck(deckTitle, app: app)
+        
+        app.tables.staticTexts[deckTitle].tap()
+        
+        createCard(deckTitle, cardTitle: cardTitle1, cardContent: cardContent1, takeScreenshot: true, app: app)
+        createCard(deckTitle, cardTitle: cardTitle2, cardContent: cardContent2, takeScreenshot: false, app: app)
+        createCard(deckTitle, cardTitle: cardTitle3, cardContent: cardContent3, takeScreenshot: false, app: app)
+        
+        snapshot("AllCards")
+        
+        startPresentation(takeScreenshot: true, app: app)
+        
+        app.navigationBars[deckTitle].buttons["QCards"].tap()
+        
+        toggleDarkMode(app)
+        
+        app.tables.staticTexts[deckTitle].tap()
+        
+        startPresentation(takeScreenshot: true, app: app)
+        
+        app.navigationBars[deckTitle].buttons["QCards"].tap()
+        
+        deleteDeck(deckTitle, app: app)
+        toggleDarkMode(app)
+    }
+    
+    // AllDecks
+    func setTimePerCard(_ app: XCUIApplication) {
         app.navigationBars["QCards"].buttons["settings"].tap()
         
         app.tables.cells.element(boundBy: 0).tap()
@@ -35,7 +71,10 @@ class FastlaneSnapshot: XCTestCase {
         
         app.navigationBars.element(boundBy: 0).buttons["saveButton"].tap()
         app.navigationBars.element(boundBy: 0).buttons["okButton"].tap()
-        
+    }
+    
+    // AllDecks
+    func createDeck(_ deckTitle: String, app: XCUIApplication) {
         app.navigationBars["QCards"].buttons["createDeckButton"].tap()
         
         let deckTitleField = app.textFields["deckTitleTextField"]
@@ -43,10 +82,10 @@ class FastlaneSnapshot: XCTestCase {
         deckTitleField.typeText(deckTitle)
         
         app.buttons["addButton"].tap()
-        
-        snapshot("AllDecks")
-        
-        app.tables.staticTexts[deckTitle].tap()
+    }
+    
+    // AllCards
+    func createCard(_ deckTitle: String, cardTitle: String, cardContent: String, takeScreenshot: Bool, app: XCUIApplication) {
         app.navigationBars[deckTitle].buttons["addButton"].tap()
         
         let cardTitleTextField = app.textFields["cardTitleTextField"]
@@ -57,21 +96,37 @@ class FastlaneSnapshot: XCTestCase {
         cardContentTextView.tap()
         cardContentTextView.typeText(cardContent)
         
-        snapshot("CreateCard")
+        if takeScreenshot {
+            snapshot("CreateCard")
+        }
         
         app.buttons["saveButton"].tap()
-        
-        snapshot("AllCards")
-        
-        app.buttons["play"].tap()
-        
-        snapshot("Presentation")
-        
-        app.buttons["stop"].tap()
-        app.navigationBars[deckTitle].buttons["QCards"].tap()
+    }
+    
+    // AllDecks
+    func deleteDeck(_ deckTitle: String, app: XCUIApplication) {
         app.tables.staticTexts[deckTitle].swipeLeft()
-        
         app.buttons.element(boundBy: 3).tap()
         app.buttons["yesButton"].tap()
+    }
+    
+    // AllDecks
+    func toggleDarkMode(_ app: XCUIApplication) {
+        app.navigationBars["QCards"].buttons["settings"].tap()
+        
+        app.switches.element(boundBy: 2).tap()
+        
+        app.navigationBars.element(boundBy: 0).buttons["okButton"].tap()
+    }
+    
+    // AllCards
+    func startPresentation(takeScreenshot: Bool, app: XCUIApplication) {
+        app.buttons["play"].tap()
+        
+        if takeScreenshot {
+            snapshot("Presentation")
+        }
+        
+        app.buttons["stop"].tap()
     }
 }
--- FastlaneSnapshot/LocalizationHelper.swift
+++ FastlaneSnapshot/LocalizationHelper.swift
@@ -8,26 +8,9 @@
 
 import Foundation
 
-func localizedString(_ key: String) -> String {
-    let testBundle = Bundle(for: FastlaneSnapshot.self)
-    if let currentLanguage = currentLanguage,
-        let testBundlePath = testBundle.path(forResource: currentLanguage.localeCode, ofType: "lproj") ?? testBundle.path(forResource: currentLanguage.langCode, ofType: "lproj"),
-        let localizedBundle = Bundle(path: testBundlePath) {
-        return NSLocalizedString(key, bundle: localizedBundle, comment: "")
-    }
-    return "?"
-}
-
-var currentLanguage: (langCode: String, localeCode: String)? {
-    let currentLocale = Locale(identifier: Locale.preferredLanguages.first!)
-    guard let langCode = currentLocale.languageCode else {
-        return nil
-    }
-    var localeCode = langCode
-    if let scriptCode = currentLocale.scriptCode {
-        localeCode = "\(langCode)-\(scriptCode)"
-    } else if let regionCode = currentLocale.regionCode {
-        localeCode = "\(langCode)-\(regionCode)"
-    }
-    return (langCode, localeCode)
+func localizedString(key: String) -> String {
+    let localizationBundle = Bundle(path: Bundle(for: FastlaneSnapshot.self)
+        .path(forResource: Locale(identifier: deviceLanguage).languageCode, ofType: "lproj")!)
+    let result = NSLocalizedString(key, bundle: localizationBundle!, comment: "")
+    return result
 }
