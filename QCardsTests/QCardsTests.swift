74a7777afc6ab0a7639558b0ed3438eab4c5bdc4
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-08-18 10:20:29 +0200

    Add unit test targets

---
+++ QCardsTests/QCardsTests.swift
@@ -0,0 +1,33 @@
+//
+//  QCardsTests.swift
+//  QCardsTests
+//
+//  Created by Andreas Lüdemann on 18/08/2019.
+//  Copyright © 2019 Andreas Lüdemann. All rights reserved.
+//
+
+import XCTest
+
+class QCardsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
