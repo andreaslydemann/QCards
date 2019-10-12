69e2c2e15b7cd5d0c7d74663045cbeeb1338f80b
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-10-12 23:47:58 +0200

    Update message of validate_branch_name.sh

--- scripts/validate_branch_name.sh
+++ scripts/validate_branch_name.sh
@@ -5,7 +5,7 @@ local_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 valid_branch_regex="^(feature|fix|hotfix|release)\/[a-z0-9._-]+$"
 
-message="Branch names in this project must adhere to this contract: $valid_branch_regex."
+message="Branch name '$local_branch' doesn't adhere to this contract: $valid_branch_regex."
 
 if [[ ! $local_branch =~ $valid_branch_regex ]]
 then
