d302897be691419fe22345d16b73b82a8ae0cb6f
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-10-12 23:40:32 +0200

    Update regex of validate_branch_name.sh

--- scripts/validate_branch_name.sh
+++ scripts/validate_branch_name.sh
@@ -3,7 +3,7 @@ LC_ALL=C
 
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_branch_regex="^(revert*|\d+-.+|(feature|fix|hotfix|release)/.+)"
+valid_branch_regex="^(feature|fix|hotfix|release)\/[a-z0-9._-]+$"
 
 message="Branch names in this project must adhere to this contract: $valid_branch_regex."
 
